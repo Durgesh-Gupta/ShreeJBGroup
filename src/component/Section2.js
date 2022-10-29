a6f1c2a6fdae6e6683f68b958537aac85b207fe1
Durgesh-Gupta <durgesh10gupta@gmail.com>
2022-10-29 20:27:40 +0530

    feat:Added Basic section component

---
+++ src/component/Section2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Section2 = () => {
+  return (
+    <div className="fullScreenContainer" style={{ backgroundColor: "blue" }}>
+      Section2
+    </div>
+  );
+};
+
+export default Section2;
---
+++ src/component/Service.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div className='fullScreenContainer' style={{backgroundColor:"red"}}>Service</div>
+  )
+}
+
+export default Service
